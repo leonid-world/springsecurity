91f07ddcbfd98ade53444272eb1d03a118e58d01
leonid <leonid.always@gmail.com>
2024-04-26 10:09:53 +0900

    AuthenticationManager

--- src/main/java/io/example/springsecurity/SecurityConfig.java
+++ src/main/java/io/example/springsecurity/SecurityConfig.java
@@ -6,6 +6,7 @@ import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.Authentication;
@@ -28,62 +29,49 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
 
         /**
+         * ** AuthenticationManager
          *
-         * *** SecurityContext ***
-         * - Authentication 저장 : 현재 인증된 사용자의 Authentication 객체를 저장하는 저장소
-         * - ThreadLocal 저장소 : SecurityContextHolder를 통해 접근되며 ThreadLocal에 Context가 저장되는데, 이 ThreadLocal 저장소를 사용해 각 스레드가 자신만의 보안 컨텍스트를 유지한다.
-         *   ex) client 3명일 때, 서버는 client 마다 스레드를 생성한다. 이 스레드가 client의 요청을 처리한다. 이 스레드마다 ThreadLocal 이란 독립적인 저장소를 갖고있다.
-         *       이 ThreadLocal에 SecurityContext 값이 저장된다. 즉, client 마다 스레드가 생성되고 스레드마다 스레드로컬이 있고 스레드로컬에 컨텍스트루트가 있다는 것은, 독립적으로 컨텍스트루트 객체를 갖고있다.
-         *       각각은 독립적이기 때문에 다른 스레드의 값을 가져오거나 저장할 순 없다. 자기만의 Authentication 객체만 참고할 수 있기 때문에, 자신만의 보안 컨텍스트를 유지한다고 볼 수 있다.
+         * 인증 필터로부터 Authentication 객체를 전달 받아 인증을 시도하며 인증에 성공할 경우 사용자 정보, 권한 등을 포함한 완전히 채워진 Authentication 객체를 반환한다.
+         *  ex) AuthenticationFilter -> Authentication 객체 생성(Username, Password 저장) -> AuthenticationManager에게 인증객체 전달 및 인증처리 위임
+         *  -> AuthenticationManager는 내부적으로 인증처리 수행 -> 인증성공? -> Authentication 객체 데이터 보강하여 다시 생성(User객체나 Authority 등 추가됨)
+         *  -> AuthenticationFilter로 다시 반환.
+         *  이 역할을 AuthenticationManager가 한다.
+         *   즉, 인증받기 전 후 과정에서 다리역할을 하는 것
          *
-         * - 애플리케이션 전반에 걸친 접근성 : 애플리케이션의 어느 곳에서나 접근 가능하고 현재 사용자의 인증 상태나 권한을 확인하는 데 사용된다.
+         * AuthenticationManager 는 여러 authenticationProvider 들을 관리하며 AuthenticationProvider 목록을 순차적으로 순회하며 인증 요청을 처리한다.
+         * ex ) AuthenticationProvider 클래스들을 가지고 있다.
+         * -> 목록 중에서 인증처리요건에 맞는 적절한 Provider를 찾아서 위임한다.
          *
-         *  SecurityContext 참조 : SecurityContextHolder.getContextHolderStrategy().getContext()
-         *  SecurityContext 삭제 : SecurityContextHolder.getContextHolderStrategy().clearContext()
+         * AuthenticationProvider 목록 중에서 인증 처리 요건에 맞는 적절한 AuthenticationProvider를 찾아 인증처리를 위임한다.
+         * 즉, 인증처리 전 인증객체를 받아서 Provider에 위임하면, 인증성공 후 Authentication 객체를 받아서 Filter에 넘겨준다.
          *
-         * **************** 자바 동시성 문제 ***********************
-         *  스레드 풀에서 운용되는 스레드는, 새로운 요청이더라도 기존의 ThreadLocal이 재사용될 수 있기 때문에 클라이언트로 응답 직전에 항상 SecurityContext를 삭제 해 줘야 한다.
-         *  스레드는 재사용되는데, Client1에게 할당된 스레드가 요청을 수행한 후, Client4에게 할당될 수 있기 때문에, Authentication 정보는 삭제해야 한다.
-         *  그래서, 매번 ThreadLocal 의 SecurityContext는 다시 인증을 수행한다.
-         *  만약, 요청이 들어올 때마다 스레드풀의 스레드가 추가로 생성된다면 초기화할 필요가 없긴 하다.
+         * AuthenticationManagerBuilder에 의해 객체가 생성되며 주로 사용하는 구현체로 ProviderManager가 제공된다.
+         * -> builder 클래스는 AuthenticationManager를 생성하는데, ProviderManager라는 구현체를 사용한다.
+         * 즉, ProviderManager는 Provider를 관리하는 객체이다.
          *
          *
-         * *** SecurityContextHolder
-         * - SecurityContext 저장 : 현재 인증된 사용자의 Authentication 객체를 담고 있는 SecurityContext객체를 저장한다.
-         *    ex) SecurityContext 객체를 저장하는 방식은 3가지가 있다. 다양한 저장 전략을 지원하기 위해 전략패턴이 사용된다.
-         * - 전략 패턴 사용 : 다양한 저장 전략을 지원하기 위해 SecurityContextHolderStrategy 인터페이스를 사용한다.
-         * - 기본 전략 : MODE_THREADLOCAL 특정 전략을 지정하지 않으면, 이 전략이 디폴트가 된다.
-         * - 전략 모드 직접 설정 : SecurityContextHOlder.setStrategyName(String 전략이름) 전략모드를 직접 설정하고 싶다면...
-         *      그리고 전략들은 각각 다른 Strategy 구현체를 생성한다.
+         * ** AuthenticationmanagerBuilder
          *
-         *   ** 구조 **
-         *   - SecurityContextHolderStrategy
-         *    setDeferredContext : 현재 컨텍스트를 반환하는 Supplier를 저장한다. (바로 메모리에서 가져오는것이 아니라, Supplier 에 저장해놓고 필요시 가져오기 때문에, 지연이 발생한다.)
-         *    createEmptyContext
-         *    getContext
-         *    clearContext
-         *    setContext
-         *    getDeferredContext : 현재 컨텍스트를 반환하는 Supplier를 얻는다.(바로 메모리에서 가져오는것이 아니라, Supplier 에 저장해놓고 필요시 가져오기 때문에, 지연이 발생한다.)
-         *          Supplier에 저장하고 필터가 이를 필요로 하는 시점에 Supplier를 실행시켜서 세션에서 context를 꺼내옴.
-         *          객체의 생성시점을 지연시킴으로서 성능상 이점을 가져갈 수 있다.
+         * AuthenticationManager 객체를 생성하며 UserDetailsService 및 AuthenticationProvider 를 추가할 수 잇다.
+         *  -> Builder 클래스를 통해, UserDetailsService 및 AuthenticationProvider를 추가한다.
+         *  AuthenticationManager로 직접 Provider를 추가하거나 생성하는것은 아니다. 오직 빌더로만 한다.
+         *  Provider를 생성하여, AuthenticationManager로 전달하는 것이다.
          *
-         * *** SecurityContextHolder 저장 모드
-         * - MODE_THREADLOCAL : 기본 모드로, 각 스레드가 독립적인 보안 컨텍스트를 가진다. 대부분의 서버 환경에 적합
-         *      -> ThreadLocalSecurityContextHolderStrategy 구현체가 생성되고 SecurityContext를 저장한다.
-         *
-         * - MODE_INHERITABLETHREADLOCAL : 부모 스레드로부터 자식 스레드로 보안 컨텍스트가 상속되며 작업을 스레드 간 분산 실행하는 경우 유용할 수 있다.
-         *    ex) 부모가 별도 스레드를 생성하면 자식에서도 표현된다.  원래는, 부모 스레드에 Security Context가 생성되면, 자식에게 상속되지 않음.
-         *       부모 스레드의 Security Context 를 참조하고 싶을 때 사용하는 모드이다.
-         *      -> InheritableThreadLocalSecurityContextHolderStrategy 구현체가 생성되고 SecurityContext를 저장.
-         *
-         * - MODE_GLOBAL : 전역적으로 단일 보안 컨텍스트를 사용하며 서버 환경에서는 부적합하며 주로 간단한 애플리케이션에 적합하다.
-         *      -> GlobalSecurityContextHolderStrategy 구현체를 생성하고 SecurityContext를 저장.
-         *
+         * - 어떻게 참조하는가?
+         * HttpSecurity.getSharedObject(AuthenticationManagerBuilder.class) 를 통해 객체를 참조할 수 있다.
+         *    !! 참고 : HttpSecurity 는 어떤 자원을 공유할 수 있고 참조할 수 있다.
+         *    getSharedObject 에 공유하고자 하는 클래스타입을 주입하면 된다.
          *
          */
 
-        HttpSessionRequestCache requestCache = new HttpSessionRequestCache();
-        requestCache.setMatchingRequestParameterName("customParam=y");
+
+        /**
+         * AuthenticationManager 사용법 - HttpSecurity 사용
+         *
+         * AuthenticationManagerBuilder authenticationManagerBuilder = http.getSharedObject(AuthenticationManagerBuilder.class);
+         * AuthenticationManager authenticationManager = authenticationManagerbuilder.build();
+         * AuthenticationManager authenticationManager = authenticationManagerBuilder.getObject(); // build()는 최초 한번만 호출해야 한다. build() 후에는 getObject() 로 참조해야 한다.
+         */
 
         http
             .authorizeHttpRequests(auth -> auth
