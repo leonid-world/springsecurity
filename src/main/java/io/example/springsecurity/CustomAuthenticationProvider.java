cf12b4ec058654b5abd3a2f775c0bdbc9abaafcf
leonid <leonid.always@gmail.com>
2024-04-30 16:28:45 +0900

    UserDetails

--- src/main/java/io/example/springsecurity/CustomAuthenticationProvider.java
+++ src/main/java/io/example/springsecurity/CustomAuthenticationProvider.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
